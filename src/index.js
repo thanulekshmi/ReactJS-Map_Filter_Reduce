ce4644725056f391c2df5653b359f81846dae1fa
Thanu <thanulekshmi@gmail.com>
2022-05-10 19:16:00 +0530

    Example for map, reduce, filter functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
+import emojipedia from "./emojipedia";
+
 var numbers = [3, 56, 2, 48, 5];
 
-//Map -Create a new array by doing something with each item in an array.
+const newNumbers = numbers.map((x) => x * 2);
+console.log(newNumbers);
 
 //Filter - Create a new array by keeping the items that return true.
+const filterNumbers = numbers.filter((x) => x > 10);
+console.log(filterNumbers);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
+const reduceNumbers = numbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
+console.log(reduceNumbers);
 
 //Find - find the first item that matches from an array.
+const findNumbers = numbers.find((x) => x > 10);
+console.log(findNumbers);
 
 //FindIndex - find the index of the first item that matches.
+const findNumbersIndex = numbers.findIndex((x) => x > 10);
+console.log(findNumbersIndex);
+
+const newArray = emojipedia.map((x) => x.meaning.substring(0, 100));
+console.log(newArray);
